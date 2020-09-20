a1865a9513370700b120f5e036bdbd43840fcd8f
keaxiang <axiang.ke@thoughtworks.com>
2020-09-20 21:03:57 +0800

    [keaxiang] add eslint and prettier

--- src/app/__tests__/app.test.js
+++ src/app/__tests__/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import App from '../app'
+import App from '../app';
 
 describe('App', () => {
   test('should render App component', () => {
